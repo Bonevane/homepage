3a5e2784aac297ae299f5c1d4aa1e457bb8c739c
Bonevane <114078747+Bonevane@users.noreply.github.com>
2025-02-12 04:12:15 +0500

    Added Hamburger Menu

--- src/index.js
+++ src/index.js
@@ -4,9 +4,31 @@ document.addEventListener("scroll", function () {
   const navbar = document.getElementById("navbar");
 
   if (window.scrollY > 10) {
-    // Adjust this value as needed
     navbar.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
   }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerBtn = document.getElementById("hamburger-btn");
+  const fullscreenMenu = document.getElementById("fullscreen-menu");
+  const closeMenuBtn = document.getElementById("close-menu");
+
+  hamburgerBtn.addEventListener("click", function () {
+    fullscreenMenu.classList.add("open");
+  });
+
+  closeMenuBtn.addEventListener("click", function () {
+    fullscreenMenu.classList.remove("open");
+  });
+
+  document.addEventListener("click", function (event) {
+    if (
+      !fullscreenMenu.contains(event.target) &&
+      !hamburgerBtn.contains(event.target)
+    ) {
+      fullscreenMenu.classList.remove("open");
+    }
+  });
 });
