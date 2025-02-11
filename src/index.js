1bd278199ade276a03aa89b3e174642e597c1047
Bonevane <114078747+Bonevane@users.noreply.github.com>
2025-02-12 03:38:35 +0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Test");
+import "./styles.css";
+
+document.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+
+  if (window.scrollY > 10) {
+    // Adjust this value as needed
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
